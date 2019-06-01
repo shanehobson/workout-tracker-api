f4aa47279763d2a188b003f19d2b4958862df7cd
Shane Hobson <shanehobson1@gmail.com>
2019-06-01 09:15:37 -0700

    Added exercise to body parts map data structure to db

--- src/models/userData.js
+++ src/models/userData.js
@@ -5,7 +5,7 @@ const userDataSchema = new mongoose.Schema({
         type: [String],
         required: true,
         trim: true,
-        default: ['Bench Press', 'Military Press', 'Bicep Curls', 'HAmmer Curls', 'Bent Over Rows', 'Abs', 'Squat', 'Tricep Pulldown', 'Seated Rows', 'Reverse Bench']
+        default: ['Bench Press', 'Military Press', 'Bicep Curls', 'Hammer Curls', 'Bent Over Rows', 'Abs', 'Squat', 'Tricep Pulldown', 'Seated Rows', 'Reverse Bench']
     },
     cardioExercises: {
         type: [String],
@@ -18,6 +18,28 @@ const userDataSchema = new mongoose.Schema({
         required: true,
         trim: true,
         default: ['Shoulders', 'Chest', 'Biceps', 'Triceps', 'Lats', 'Upper Back', 'Lower Back', 'Abs', 'Legs']
+    },
+    bodyPartsMap: {
+        type: Map,
+        required: true,
+        default: {
+            'Bench Press': ['Chest'],
+            'Military Press': ['Shoulders'],
+            'Bicep Curls': ['Biceps'],
+            'Hammer Curls': ['Biceps'],
+            'Bent Over Rows': ['Upper Back'],
+            'Abs': ['Abs'],
+            'Squat': ['Legs'],
+            'Tricep Pulldown': ['Trcipes'],
+            'Seated Rows': ['Upper Back'],
+            'Reverse Bench': ['Upper back'],
+            'Run': ['Legs'],
+            'Swim': ['Legs', 'Shoulders', 'Abs', 'Upper Back', 'Lower Back'],
+            'Hike': ['Legs'],
+            'Kayak': ['Upper Back', 'Lower Back', 'Abs'],
+            'Walk': ['Legs'],
+            'Bicycle': ['Legs']
+        }
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
